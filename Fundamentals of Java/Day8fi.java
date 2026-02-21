dc23fc0b03194237f29abd03db77f8620826184d
sabarivasanndev <sabarivasann.dev@gmail.com>
2026-02-21 21:56:09 +0530

    created

---
+++ Fundamentals of Java/Day8fi.java
@@ -0,0 +1,8 @@
+class Day8fi
+{
+    public static void main(String[] args) {
+       int[] x= new int[] {10,20,30};
+       System.out.println(x.length);
+
+    }
+}
---
+++ Fundamentals of Java/Day8fo.java
@@ -0,0 +1,8 @@
+class day8fo
+{
+    public static void main(String[] args) {
+        String x="sabari";
+        System.out.println(x.length());
+
+    }
+}
