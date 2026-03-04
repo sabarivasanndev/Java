4561f5df3ebb23cd07ea3c56a6d5c06affa4a169
sabarivasanndev <sabarivasann.dev@gmail.com>
2026-03-04 20:10:03 +0530

    create

---
+++ Day11f.java
@@ -0,0 +1,8 @@
+class Day11f
+{
+    public static void main(String[] args)
+    {
+        int n = Integer.parseInt(args[0]);
+        System.out.println("The square of " + n + " is: " + (n * n));
+    }
+}
---
+++ Day12f.java
@@ -0,0 +1,9 @@
+class day12s
+{
+    public static void main(String[]args)
+    {
+        int x=10;
+        int y=++x;
+        System.out.println(y);
+    }
+}
